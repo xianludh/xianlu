66fe30fb758e7c0d94d7ffeeb295ac2d90221c48
unpack <123456@qq.com>
2022-01-08 20:28:18 +0800

    kkqp

--- kkqp/js/script.js
+++ kkqp/js/script.js
@@ -38,7 +38,10 @@ var certifySwiper = new Swiper('#certify .swiper-container', {
   centeredSlides: true,
   loop: true,
   loopedSlides: 4,
-  autoplay: true,
+  autoplay: {
+    disableOnInteraction: false, // 鼠标滑动后继续自动播放
+    delay: 2000, //4秒切换一次
+  },
   on: {
     progress: function (progress) {
       for (i = 0; i < this.slides.length; i++) {
