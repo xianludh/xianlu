14c03a92702eaad0fa129d3e5c5abc7fd5a38596
unpack <123456@qq.com>
2022-01-18 13:46:13 +0800

    kkqp

--- kkqp/js/pc.js
+++ kkqp/js/pc.js
@@ -3,38 +3,15 @@ var certifySwiper = new Swiper('#certify .swiper-container', {
     slidesPerView: 'auto',
     centeredSlides: true,
     loop: true,
-    loopedSlides: 4,
+    loopedSlides: 3,
+    spaceBetween:38,
     autoplay: {
       disableOnInteraction: false, // 鼠标滑动后继续自动播放
       delay: 2000, //4秒切换一次
     },
-    on: {
-      progress: function (progress) {
-        for (i = 0; i < this.slides.length; i++) {
-          var slide = this.slides.eq(i);
-          var slideProgress = this.slides[i].progress;
-          modify = 1;
-          if (Math.abs(slideProgress) > 1) {
-            modify = (Math.abs(slideProgress) - .7) * 0.4 + .6;
-          }
-          translate = slideProgress * modify * 17.33 + 'px';
-          scale = 1 - Math.abs(slideProgress) / 9;
-          zIndex = 999 - Math.abs(Math.round(10 * slideProgress));
-          slide.transform('translateX(' + translate + ') scale(' + scale + ')');
-          slide.css('zIndex', zIndex);
-          slide.css('opacity', 1);
-          if (Math.abs(slideProgress) > 3) {
-            slide.css('opacity', 0);
-          }
-        }
-      },
-      setTransition: function (transition) {
-        for (var i = 0; i < this.slides.length; i++) {
-          var slide = this.slides.eq(i)
-          slide.transition(transition);
-        }
-  
-      }
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     }
-  
+
   })
